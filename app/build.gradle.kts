6af58b756db29b55ee794c35e1e192932e225c36
youthparliament57-oss <youthparliament57@gmail.com>
2026-08-12 22:52:58 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+kotlin("jvm")
+application
+}
+
+group = "com.jarvis"
+version = "0.1.0"
+
+repositories {
+mavenCentral()
+}
+
+kotlin {
+jvmToolchain(21)
+}
+
+application {
+mainClass.set("com.jarvis.core.application.JarvisApplicationKt")
+}
+
+dependencies {
+testImplementation(kotlin("test"))
+}
+
+tasks.test {
+useJUnitPlatform()
+}
